998aa1dfa2b6e6f8d30705e5dfa0573a6c606677
Dejan22 <dejan.tomasevic@protonmail.com>
2018-01-22 19:21:29 +0100

    aded body Parser to index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const { batches, users, essions,}
 
 const PORT = process.env.PORT || 3030
 
 let app = express()
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
