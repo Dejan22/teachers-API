befb867171dfe228f7ee452a5e3b403e01155253
Dejan22 <dejan.tomasevic@protonmail.com>
2018-01-22 19:25:36 +0100

    added some errors to index file

--- index.js
+++ index.js
@@ -9,9 +9,24 @@ let app = express()
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
 
+app.use(( req,res,next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+app.use((err, req, res, next) =>{
+  res.status(err.status || 500)
+  res.json({
+    error: app.get('env') === 'development' ? err : {}
+  })
+})
+
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
 })
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
